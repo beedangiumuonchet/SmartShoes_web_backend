247a469060245e7d5adaa8cb88b6663ce2cbd8d6
/din bee/ <dinbeedymc@gmail.com>
2025-11-26 04:46:34 +0700

    fix API AI

--- src/main/java/com/ds/project/common/entities/dto/response/AiSearchResponse.java
+++ src/main/java/com/ds/project/common/entities/dto/response/AiSearchResponse.java
@@ -2,16 +2,19 @@ package com.ds.project.common.entities.dto.response;
 
 import lombok.Data;
 import java.util.List;
+import com.ds.project.app_context.models.*;
 
 @Data
 public class AiSearchResponse {
     private String mode;
     private List<ResultItem> results;
-
+    private List<String> suggestions; // có thể null khi không fallback
     @Data
     public static class ResultItem {
         private String product_id;
         private String text;
         private Double score;
+        private Integer stock;
+        private Product.Status status ;
     }
 }
